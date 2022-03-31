a660f7c39b3a6fbba7f1cac18ea0fe35800ffc35
AntonTeplyakov <jsxscript@gmail.com>
2022-03-31 15:53:42 +1000

    add CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :questions, only: %i[index new edit create update destroy show]
+  
+  # get '/questions', to: 'questions#index'
+  # get '/questions/new', to: 'questions#new'
+  # get '/questions/:id/edit', to: 'questions#edit'
+  # post '/questions', to: 'questions#create'
+
   root 'pages#index'
 end
